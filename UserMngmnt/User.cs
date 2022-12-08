b31c1e84708e325f4b66d988f9674291e376b9ad
23gillam0132 <120129008+23gillam0132@users.noreply.github.com>
2022-12-08 13:21:12 -0500

    Initial commit

---
+++ UserMngmnt/User.cs
@@ -0,0 +1,96 @@
+﻿using UserMngmnt;
+
+List<User> users = new();
+Home();
+void Home()
+{
+    Console.Clear();
+    Console.WriteLine("Welcome.");
+    Console.WriteLine("1. Create user.");
+    Console.WriteLine("2. Login.");
+    Console.WriteLine("3. View Users.");
+    string input = Console.ReadLine();
+    switch(input)
+    {
+        case "1":
+            CreateUser();
+            break;
+        case "2":
+            Login();
+            break;
+        case "3":
+            ViewUsers();
+            break;
+    }
+}
+void CreateUser()
+{
+    Console.WriteLine("Create a username");
+    string inputUser = Console.ReadLine();
+    Console.WriteLine("Create a password");
+    string inputPw = Console.ReadLine();
+    Console.WriteLine("Choose 'Admin' 'User' 'Guest'");
+    string input = Console.ReadLine();
+    if (String.Equals(input, "Admin", StringComparison.CurrentCultureIgnoreCase))
+    {
+        users.Add(new Administrator(inputUser, inputPw));
+    } else if (String.Equals(input, "User", StringComparison.CurrentCultureIgnoreCase))
+    {
+        users.Add(new User(inputUser, inputPw));
+    } else
+    {
+        users.Add(new Guest(inputUser, inputPw));
+    }
+    
+    Console.WriteLine("Created user");
+    Console.ReadKey();
+    Home();
+}
+void Login()
+{
+    Console.WriteLine("Type a username to log in.");
+    foreach(User user in users)
+    {
+        Console.WriteLine(user.user);
+    }
+    string input = Console.ReadLine();
+    foreach(User user in users)
+    {
+        if (input == user.user)
+        {
+
+        }
+    }
+}
+
+void ViewUsers()
+{
+    Console.WriteLine("Admins:");
+    foreach (User user in users)
+    {
+        
+        if (user is Administrator)
+        {
+            Console.WriteLine(user.user);
+        }
+        
+    }
+    Console.WriteLine("Users:");
+    foreach (User user in users)
+    {
+        if (user is User)
+        {
+            Console.WriteLine(user.user);
+        }
+    }
+    Console.WriteLine("Guests:");
+    foreach (User user in users)
+    {
+        if (user is Guest)
+        {
+            Console.WriteLine(user.user);
+        }
+    }
+    Console.ReadKey();
+    Home();
+}
---
+++ UserMngmnt/UserClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMngmnt
+{
+    public class User
+    {
+        public string user;
+        public string pass;
+
+        public User(string user, string pass)
+        {
+            this.user = user;
+            this.pass = pass;
+        }
+    }
+    public class Guest : User
+    {
+        public Guest(string user, string pass) : base(user, pass)
+        {
+        }
+    }
+    public class Administrator : User
+    {
+        public Administrator(string user, string pass) : base(user, pass)
+        {
+        }
+    }
+}
